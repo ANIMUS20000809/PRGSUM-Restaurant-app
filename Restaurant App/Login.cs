2052033238dea2258c9e346d5f5a281b62c5fd18
Richard Berthold <5139@ctucareer.co.za>
2019-06-09 07:49:09 +0200

    Fixed Login Form

--- Restaurant App/Login.cs
+++ Restaurant App/Login.cs
@@ -13,13 +13,12 @@ namespace Restaurant_App
 {
     public partial class Login : Form
     {
-        public string constr = "Data Source=ANIMUS;Initial Catalog=Restaurant;Integrated Security=True";
         public Login()
         {
             InitializeComponent();
         }
+
         #region Methods
-        // TODO: Search the user name from the fucking database!!!!
         /// <summary>
         /// Checks the username
         /// </summary>
@@ -27,45 +26,67 @@ namespace Restaurant_App
         {
             try
             {
-                switch (usrbx.Text)
+                if (usrbx.Text == "Admin")
                 {
-                    case "Admin":
-                        SessionContext.Role = SessionContext.SessionContextRole.Admin;
-                        MessageBox.Show($"Welcome back {SessionContext.Role}!");
-                        Admin_page a = new Admin_page();
-                        a.ShowDialog();
-                        break;
+                    SessionContext.Role = SessionContext.SessionContextRole.Admin;
+                    MessageBox.Show($"Welcome back {SessionContext.Role}!");
+                    Admin_page a = new Admin_page();
+                    a.ShowDialog();
+                }
+                else
+                {
+                    using (SqlConnection con = new SqlConnection(SessionContext.ConnectionString))
+                    {
+                        con.Open();
 
-                    case "Richard":
-                        SessionContext.Role = SessionContext.SessionContextRole.Customer;
-                        MessageBox.Show($"Welcome back {SessionContext.Role}!");
-                        C_Book c = new C_Book();
-                        c.ShowDialog();
-                        break;
+                        if (con.State == ConnectionState.Open)
+                        {
+                            using (SqlCommand com = new SqlCommand($"SELECT Name, Role FROM Accounts WHERE Name = '{usrbx.Text}'", con))
+                            {
+                                using (SqlDataReader rd = com.ExecuteReader())
+                                {
+                                    if (rd.HasRows)
+                                    {
+                                        rd.Read();
+                                        switch (rd["Role"].ToString())
+                                        {
+                                            case "Customers":
+                                                SessionContext.Role = SessionContext.SessionContextRole.Customer;
+                                                MessageBox.Show($"Welcome back {SessionContext.Role}!");
+                                                C_Book c = new C_Book();
+                                                c.ShowDialog();
+                                                break;
 
-                    case "Donovan":
-                        SessionContext.Role = SessionContext.SessionContextRole.Waiter;
-                        MessageBox.Show($"Welcome back {SessionContext.Role}!");
-                        W_Tables w = new W_Tables();
-                        w.ShowDialog();
-                        break;
+                                            case "Waiters":
+                                                SessionContext.Role = SessionContext.SessionContextRole.Waiter;
+                                                MessageBox.Show($"Welcome back {SessionContext.Role}!");
+                                                W_Tables w = new W_Tables();
+                                                w.ShowDialog();
+                                                break;
 
-                    default:
-                        MessageBox.Show("Username or password is incorrect");
-                        break;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("Username or password incorrect", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                    }
+                                }
+                            }
+                        }
+                    } 
                 }
             }
             catch (FormatException fEx)
             {
-                MessageBox.Show(fEx.Message);
+                MessageBox.Show(fEx.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             catch (InvalidOperationException iex)
             {
-                MessageBox.Show(iex.Message);
+                MessageBox.Show(iex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             catch (Exception ex)
             {
-                MessageBox.Show("An error ocurred. Please try again: " + ex.Message);
+                MessageBox.Show("An error ocurred. Please try again: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
         /// <summary>
@@ -110,8 +131,8 @@ namespace Restaurant_App
         {
             enter(e);
         }
-        #endregion
 
+        #endregion
 
     }
 }
